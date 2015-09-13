2ed7adcfe279a1c0b518a1aeeeb6dde9cf386bb6
ThomasTiny <thomas@team-swordfish.com>
2015-09-13 11:07:33 +0800

    Update size of legend.

--- plot4.R
+++ plot4.R
@@ -4,8 +4,8 @@ library(zoo)
 #plot setting:
 # 1 row 1 column
 # square plotting region
-par(mar=c(4,4,2,2), mfrow=c(2,2), pty = "s")
-z <- read.zoo("household_power_consumption.txt", sep=";", header=TRUE, format = "%d/%m/%Y")
+par(mar=c(4,4,2,2), mfrow=c(2,2), pty = "s", cex='0.6')
+#z <- read.zoo("household_power_consumption.txt", sep=";", header=TRUE, format = "%d/%m/%Y")
 sub_z <- window(z, start = "2007-02-01", end = "2007-02-02")
 dateTime <- ymd_hms(paste(index(sub_z[,1]), sub_z[,1]))
 
